845314a62546c19e058d66e401b1834d2d19ee43
Sergio Herman <sergioherman@id.uff.br>
2023-11-08 19:29:39 -0300

    Alteração no Javascript da Header para aparecer ou esconder menus dos botçoes ao passar o mouse ou iniciar toque na tela. Reorganização de código da header. Adição de imagens nos recursos. Melhoria na estilização dos botões e mapa no footer.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,6 +3,11 @@ let menuAtivo = 0;
 const hamburgerIcon = document.querySelector('.hamb-icon');
 const menu = document.querySelector('.div-btns-barra');
 const menuBtn = document.querySelectorAll('.menu-btn');
+const botoes = document.querySelectorAll('.btn-barra');
+
+// const areaMenu = document.querySelector('.area-menu');
+
+
 // console.log(menuBtn);
 hamburgerIcon.addEventListener('click', function(){
     // console.log('ctrlMenu chamado');
@@ -40,10 +45,12 @@ window.addEventListener('resize', function(){
     }
 });
 
+
 document.addEventListener('click', function(event) {
     if (menu.contains(event.target) || hamburgerIcon.contains(event.target)) {
-        // console.log('Clique dentro do menu!');
+        console.log('Clique dentro do menu!');
     }else {
+        console.log('Clique fora do menu!');
         if(window.innerWidth <= 768){
             menu.style.display = 'none';
             for (let i = 0; i < menuBtn.length; i++){
@@ -54,37 +61,95 @@ document.addEventListener('click', function(event) {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const botoes = document.querySelectorAll('.btn-barra');
-    const menus = document.querySelectorAll('.menu-btn');
 
-    console.log(botoes);
-    console.log(menus);
+
+
+
+
+
+
+
+
+
+
+// ****** BUSCAR MANEIRA DE REMOVER OU ADICIONAR LISTENER QUANDO A TELA ALTERAR O TAMANHO A INVER DE RECARREGAS A PAGINA INTEIRA ********************************
+window.onresize = function(event) {
+    location.reload(); // Recarrega a página quando a janela é redimensionada
+};
+// Precisa ser executado se a pagina sofrer variação de tamanho para adicionar ou remover os listeners abaixo
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
 
     botoes.forEach(function(botao, index) {
-        botao.addEventListener('click', function() {
-
-
+        botao.addEventListener('mouseover', function() {
             if(window.innerWidth > 768){
-                console.log("botao clicado");
+                // console.log("cursor entrou");
                 // Fecha todos os menus
-                menus.forEach(function(menu) {
+                menuBtn.forEach(function(menu) {
                     menu.style.display = 'none';
-                    console.log("escondeu");
+                    // console.log("escondeu");
                 });
                 // Abre o menu associado ao botão clicado
-                menus[index].style.display = 'flex';            
+                menuBtn[index].style.display = 'flex';            
             }
-
-
 
         });
     });
 
+
+    menuBtn.forEach(function(menu, index){
+        if(window.innerWidth > 768){
+            menu.addEventListener('mouseleave', function() {
+                // console.log("mouse fora da area do menu");
+                menu.style.display = 'none';
+            });
+        }
+    });
+    
+
+
+    botoes.forEach(function(botao, index) {
+        botao.addEventListener('touchstart', function() {
+            if(window.innerWidth > 768){
+                // console.log("cursor entrou");
+                // Fecha todos os menus
+                menuBtn.forEach(function(menu) {
+                    menu.style.display = 'none';
+                    // console.log("escondeu");
+                });
+                // Abre o menu associado ao botão clicado
+                menuBtn[index].style.display = 'flex';            
+            }
+
+        });
+    });
+
+
+    menuBtn.forEach(function(menu, index){
+        if(window.innerWidth > 768){
+            menu.addEventListener('touchend', function() {
+                // console.log("mouse fora da area do menu");
+                menu.style.display = 'none';
+            });
+        }
+    });
+    
+
+
+
+
+
+
+
     // Adiciona um ouvinte de clique ao documento para fechar o menu se clicar fora do botão/menus
     document.addEventListener('click', function(event) {
         if (!event.target.matches('.div-btns-barra .btn-barra') && window.innerWidth > 768) {
-            menus.forEach(function(menu) {
+            menuBtn.forEach(function(menu) {
                 menu.style.display = 'none';
             });
         }
