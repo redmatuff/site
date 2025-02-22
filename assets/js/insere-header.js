82851a3eb06cdfea59ffa06a1815061d1d91ec3c
GabrielHCRJ <gabrieldomingues@id.uff.br>
2025-02-22 20:05:43 -0300

    site versão 2025.1

--- assets/js/insere-header.js
+++ assets/js/insere-header.js
@@ -9,9 +9,14 @@ header.innerHTML = `
                 
                 <!-- Header 1.1.1. Logo -->
                 <a href="https://redmatuff.github.io/site/index.html">
-                    <!-- <img src="../assets/icons/logo-transparente.png" alt="logo REDMat" class="logo"> -->
+                    <!-- <img src="https://redmatuff.github.io/site/assets/icons/logo-transparente.png" alt="logo REDMat" class="logo"> -->
                     <img src="https://redmatuff.github.io/site/assets/icons/logo-transparente.png" alt="logo REDMat" class="logo">
                 </a>
+
+                    <!--const img = document.querySelector("header img");
+                    if (img) {
+                    img.style.setProperty("width", "50%", "important");
+                    }-->
 
                 <!-- Header 1.1.2. Títulos Longos e Curtos -->
                 <div class="titulo-barra">
@@ -42,8 +47,6 @@ header.innerHTML = `
             <!-- Header 1.2. Barra de Botões ou Menu -->
             <div class="div-btns-barra">
 
-
-
                 <!-- Botão 1 -->
                 <div>
                     <a href="https://redmatuff.github.io/site/pages/quem-somos.html">
@@ -68,82 +71,49 @@ header.innerHTML = `
 
                 <!-- Botão 3 -->
                 <div>
-                    <a href="https://redmatuff.github.io/site/pages/proj-ensino.html">
-                        <button class="btn-barra">Projetos de Ensino</button>
-                    </a>                   
+                    <a href="https://redmatuff.github.io/site/pages/pre-calculo.html">
+                        <button class="btn-barra">Pré-Cálculo</button>
+                    </a>
                     <div class="menu-btn">
-                        <!-- Acho que fica poluido, pois essa parte é po
-                        <a href="https://redmatuff.github.io/site/pages/proj-ensino.html#manim">Recursos Computacionais com Python</a>
-                        <a href="https://redmatuff.github.io/site/pages/proj-ensino.html#fcg">FCG (2023--)</a>
-                        <a href="https://redmatuff.github.io/site/pages/proj-ensino.html#novas-tec-ensino">Tecnologias de ensino (2020--)</a>
-                        <a href="https://redmatuff.github.io/site/pages/proj-ensino.html#geog-3d">Geogebra 3D (2020-2021)</a>
-                        <a href="https://redmatuff.github.io/site/pages/proj-ensino.html#gacv">GACV (2020-2022)</a>
-                        <a href="https://redmatuff.github.io/site/pages/proj-ensino.html#disc-dist-soc">Disciplinas no distanciamento social (2020)</a>
-                        -->
+
                     </div>
                 </div>
                 <!-- Fim do Botão 3 -->
 
                 <!-- Botão 4 -->
                 <div>
-                    <a href="https://redmatuff.github.io/site/pages/livros-eletronicos.html">
-                        <button class="btn-barra">Livros e apostilas</button>
-                    </a>        
+                    <a href="https://redmatuff.github.io/site/pages/calculo.html">
+                        <button class="btn-barra">Cálculo</button>
+                    </a>
                     <div class="menu-btn">
-                        <a href="https://www.geogebra.org/m/hzvsftdf">Calculo com Geobebra 3D vol. I</a>
-                        <a href="https://www.geogebra.org/m/jke7hpfs">Calculo com Geobebra 3D vol. II</a>
-                        <a href="https://www.geogebra.org/m/wtmkn9nv">Geometria Analítica com Geogebra</a>
-                        <a href="https://redmatuff.github.io/calculovariasvariaveis/assets/pdf/apostila/apostila-completa.pdf">Apostila Cálculo Diferencial de Várias Variáveis</a>
-                        <a href="https://fcgeometria.notion.site/fcgeometria/Fundamentos-de-C-lculo-e-Geometria-d169bff4f8e34c229fdd97b514ff02e4
-                        ">Apostila de FGCV</a>
+
                     </div>
                 </div>
                 <!-- Fim do Botão 4 -->
 
                 <!-- Botão 5 -->
                 <div>
-                    <!-- <a href="https://redmatuff.github.io/site/pages/plataformas.html"> -->
-                        <button class="btn-barra">Plataformas</button>
-                    <!-- </a> -->
-                    
-                    <div class="menu-btn">
-                        <a href="http://www.gma.uff.br/calculo1/">Site Cálculo 1 </a>
-                        <a href="https://redmatuff.github.io/calculovariasvariaveis/">Site Cálculo 2B </a>
-                        <a href="http://www.gma.uff.br/calculo3/">Site Cálculo 3 </a>
-                    </div>               
-                </div>
-                <!-- Fim do Botão 5 -->
-                
-                <!-- Botão 6 -->
-                <div>
-                    <a href="https://redmatuff.github.io/site/pages/recursos.html">
-                        <button class="btn-barra">Recursos Comp.</button>
-                    </a>
-                    
-                    <div class="menu-btn">
-                        <a href="https://redmatuff.github.io/site/pages/recursos.html#func-uma-var">Funções de Uma Variável</a>
-                        <a href="https://redmatuff.github.io/site/pages/recursos.html#calc-func-uma-var">Cálculo de Funções de Uma Variável</a>
-                        <a href="https://redmatuff.github.io/site/pages/recursos.html#calc-func-varias-var">Cálculo de Funções de Várias Variáveis</a>
-                        <a href="https://redmatuff.github.io/site/pages/recursos.html#edo">Equações Diferenciais Ordinárias</a>
-                        <a href="https://redmatuff.github.io/site/pages/recursos.html#ga">Geometria analítica</a>
-                    </div>               
-                </div>
-                <!-- Fim do Botão 6 -->
-                
-                <!-- Botão 7 (comentado) -->
-                <!--
-                <div>
-                    <a href="https://redmatuff.github.io/site/pages/projetos-extensao.html">
-                        <button class="btn-barra">Projetos de Extensão</button>
+                    <a href="https://redmatuff.github.io/site/pages/equacoes-diferenciais.html">
+                        <button class="btn-barra">Equações Diferenciais</button>
                     </a>
                     <div class="menu-btn">
-                        <a href="">Projeto 1</a>
-                        <a href="">Projeto 2</a>
-                        <a href="">Projeto 3</a>
+
                     </div>
                 </div>
-                -->
-                <!-- Fim do Botão 7 -->
+                <!-- Fim do Botão 5 -->
+
+                <!-- Botão 6 -->
+                <div>
+                    <a href="https://redmatuff.github.io/site/pages/Cursos.html">
+                        <button class="btn-barra">Cursos</button>
+                    </a>
+                    <div class="menu-btn">
+
+                    </div>
+                </div>
+                <!-- Fim do Botão 6 -->
+
+               
 
             </div>
 
@@ -154,30 +124,5 @@ header.innerHTML = `
 
         </div>
 
-
-        <!-- Header 2. Banner -->
-        <div class="banner-container">
-
-            <!-- Header 2.1. Imagem do Banner -->
-            <img class="banner-img" src="https://redmatuff.github.io/site/assets/img/banner-redmat%20V5%20brilho.png" alt="banner">
-
-            <!-- Header 2.2. Textos do Banner -->
-            <div class="banner-txt-container">
-                <div class = "txt-banner">
-                    <a href="https://www.uff.br/">                   
-                        <h4>Universidade Federal Fluminense</h4>
-                    </a>
-                    <a href="https://ime.uff.br/">
-                        <h4>Instituto de Matemática e Estatística</h4>
-                    </a>
-                    <a href="https://redmatuff.github.io/site/index.html">
-                        <h1>REDMat</h1>
-                    </a>
-                    <hr>
-                    <h5 class="titulo-dir">Recursos&nbsp;Educacionais&nbsp;Digitais para&nbsp;o&nbsp;ensino&nbsp;da&nbsp;Matemática</h5>
-                </div>
-            </div>
-
-        </div>
 
 `
